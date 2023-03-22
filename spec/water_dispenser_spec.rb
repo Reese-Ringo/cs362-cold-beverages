8e9bee104a49877e8f0986c8b7024f34b553c40f
Reese Ringo <ringor@oregonstate.edu>
2023-03-21 20:14:34 -0700

    Dispenser under test

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -2,4 +2,17 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
 
+    it 'has a reservoir' do
+        reservoir = WaterReservoir.new
+        dispenser = WaterDispenser.new(reservoir)
+        expect(reservoir).to eq(dispenser.reservoir)
+    end
+
+    it 'can drain a reservoir' do
+        reservoir = WaterReservoir.new(10, 10) # Initialize as full
+        dispenser = WaterDispenser.new(reservoir)
+        vessel = Vessel.new('Bottle', 2)
+        dispenser.dispense(vessel)
+        expect(reservoir.current_water_volume).to eq(8)
+    end
 end
